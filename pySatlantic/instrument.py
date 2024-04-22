13db86109bcf63bebfec7354418633f38580a85b
Nils <nils.haentjens@maine.edu>
2024-04-22 18:35:20 -0400

    Replace f-string by str.format for compatibility with python3.5
    - bump to version 0.4.2

--- pySatlantic/instrument.py
+++ pySatlantic/instrument.py
@@ -62,10 +62,10 @@ def sat_dtype_to_np_dtype(sat_data_type, sat_field_length):
     if sat_data_type == 'AS':
         # ASCII string (text)
         if sat_field_length:
-            return f'<{sat_field_length}U'
+            return '<{}U'.format(sat_field_length)
         else:
             # Assume maximum size of 12 characters
-            return f'<12U'
+            return '<12U'
     elif sat_data_type == 'AI':
         # ASCII integer number
         return np.int32
@@ -97,7 +97,7 @@ def sat_dtype_to_np_dtype(sat_data_type, sat_field_length):
         # double float
         return np.float64
     else:
-        raise ParserTypeError(f'Unknown format decoder {sat_data_type}{sat_field_length}')
+        raise ParserTypeError('Unknown format decoder {}{}'.format(sat_data_type, sat_field_length))
 
 
 class Parser:
@@ -359,7 +359,7 @@ class Parser:
             if self.variable_frame_length:
                 for t in self.data_type:
                     if t not in ['AS', 'AI', 'AF']:
-                        raise ParserTypeError(f"Invalid data type {t} for variable frame length.")
+                        raise ParserTypeError("Invalid data type {} for variable frame length.".format(t))
 
             # Special case of NMEA_CHECKSUM (not documented in SAT-DN-00134)
             #       data-type should be AI but it's an hexadecimal int instead of int.
@@ -367,7 +367,7 @@ class Parser:
                 i = self.key.index('NMEA_CHECKSUM')
                 self.type[i] = 'CHECK'
                 self.id[i] = 'SUM'
-                self.key[i] = f'{self.type}_{self.id}'
+                self.key[i] = '{}_{}'.format(self.type, self.id)
                 self.data_type[i] = 'AI16'
 
             # Specify end of check sum computation
@@ -382,11 +382,11 @@ class Parser:
             self.core_variables = [i for i, (x, y) in enumerate(zip(self.type, self.fit_type))
                                    if x.upper() in self.CORE_VARIABLE_TYPES and y != 'NONE']
             if self.core_variables:
-                self.core_groupname = f'{self.type[self.core_variables[0]]}'
+                self.core_groupname = str(self.type[self.core_variables[0]])
             self.unusable_variables = [i for i, (x, y) in enumerate(zip(self.type, self.fit_type))
                                        if x.upper() in self.CORE_VARIABLE_TYPES and y == 'NONE']
             if self.unusable_variables:
-                self.unusable_groupname = f'{self.type[self.core_variables[0]]}_RAW'
+                self.unusable_groupname = '{}_RAW'.format(self.type[self.core_variables[0]])
             self.auxiliary_variables = [i for i, (x, y) in enumerate(zip(self.type, self.fit_type)) if
                                         x.upper() not in self.CORE_VARIABLE_TYPES]
 
@@ -400,18 +400,18 @@ class Parser:
             for data, fit in zip(self.data_type, self.fit_type):
                 if fit in ['OPTIC1', 'DDMMYY']:
                     if data not in ['BU', 'BS', 'AI']:
-                        raise ParserTypeError(f"Valid data types for OPTIC1 are BU, BS, and AI.")
+                        raise ParserTypeError("Valid data types for OPTIC1 are BU, BS, and AI.")
                 elif fit in ['GPSTIME', 'GPSPOS', 'DDMM', 'HHMMSS']:
                     if data not in ['BF', 'BD', 'AF']:
-                        raise ParserTypeError(f"Valid data types for {fit} are BF, BD, and AF.")
+                        raise ParserTypeError("Valid data types for {} are BF, BD, and AF.".format(fit))
                 elif fit in ['OPTIC2', 'OPTIC3', 'THERM1', 'POW10', 'POLYU', 'POLYF', 'TIME2']:
                     if data == 'AS':
-                        raise ParserTypeError(f"All data types are valid with {fit}, except AS.")
+                        raise ParserTypeError("All data types are valid with {}, except AS.".format(fit))
                 elif fit in ['GPSHEMI', 'GPSMODE', 'GPSSTATUS']:
                     if data != 'AS':
-                        raise ParserTypeError(f"The only valid data type for {fit} is AS.")
+                        raise ParserTypeError("The only valid data type for {} is AS.".format(fit))
                 elif fit not in ['COUNT', 'NONE', 'DELIMITER']:
-                    raise ParserFitError(f"Fit type {fit} not supported.")
+                    raise ParserFitError("Fit type {} not supported.".format(fit))
 
     @staticmethod
     def _sat_dtype_to_struct(sat_data_type, sat_field_length):
@@ -451,22 +451,22 @@ class Parser:
             # double float
             return 'd'
         else:
-            raise ParserTypeError(f'Unknown byte decoder combination {sat_data_type} {sat_field_length}')
+            raise ParserTypeError('Unknown byte decoder combination {} {}'.format(sat_data_type, sat_field_length))
 
     def __str__(self):
-        return f'{self.frame_header}\n' + \
-               f'\tInstrument: {self.instrument}\n' + \
-               f'\tSerial number: {self.sn}\n' + \
-               f'\tNumber of fields: {self.frame_nfields}\n' + \
-               f'\tVariable frame length: {self.variable_frame_length}\n' + \
-               f'\tFrame length (in bytes): {self.frame_length}\n' + \
-               f'\tVariables type: {self.type}\n' + \
-               f'\tVariables id: {self.id}\n' + \
-               f'\tVariables units: {self.units}\n' + \
-               f'\tVariables fit type:{self.fit_type}\n' + \
-               f'\tVariables field length: {self.field_length}\n' + \
-               f'\tVariables data type: {self.data_type}\n' + \
-               f'\tVariables frame format: {self.frame_fmt}\n'
+        return '{}\n'.format(self.frame_header) + \
+               '\tInstrument: {}\n'.format(self.instrument) + \
+               '\tSerial number: {}\n'.format(self.sn) + \
+               '\tNumber of fields: {}\n'.format(self.frame_nfields) + \
+               '\tVariable frame length: {}\n'.format(self.variable_frame_length) + \
+               '\tFrame length (in bytes): {}\n'.format(self.frame_length) + \
+               '\tVariables type: {}\n'.format(self.type) + \
+               '\tVariables id: {}\n'.format(self.id) + \
+               '\tVariables units: {}\n'.format(self.units) + \
+               '\tVariables fit type:{}\n'.format(self.fit_type) + \
+               '\tVariables field length: {}\n'.format(self.field_length) + \
+               '\tVariables data type: {}\n'.format(self.data_type) + \
+               '\tVariables frame format: {}\n'.format(self.frame_fmt)
 
 
 class Instrument:
@@ -506,9 +506,9 @@ class Instrument:
                 elif ext in self.VALID_CAL_EXTENSIONS:
                     self.read_calibration_file(f, immersed)
                 else:
-                    raise CalibrationFileExtensionError(f'File extension incorrect: {f}')
+                    raise CalibrationFileExtensionError('File extension incorrect: {}'.format(f))
             else:
-                raise FileNotFoundError(f'No such file or directory: {f}')
+                raise FileNotFoundError('No such file or directory: {}'.format(f))
 
     def read_calibration_file(self, filename, immersed=False):
         ext = os.path.splitext(filename)[1]
@@ -517,7 +517,7 @@ class Instrument:
             self.cal[foo.frame_header] = foo
             self.max_frame_header_length = max(self.max_frame_header_length, len(foo.frame_header))
         else:
-            raise CalibrationFileExtensionError(f'File extension incorrect: {f}')
+            raise CalibrationFileExtensionError('File extension incorrect: {}'.format(f))
 
     def read_calibration_dir(self, dirname, immersed=False):
         empty_dir = True
@@ -919,7 +919,7 @@ class Instrument:
             d = int(value / 100)
             m = int(value - d * 100)
             s = int(((value - d * 100) - m) * 60)
-            return f'{d} {m}\' {s}\'\''
+            return '{} {}\' {}\'\''.format(d, m, s)
         elif fit_type == 'HHMMSS':
             # Universal Coordinated Time of GPS data in hours, minutes, and seconds.
             # DIVERGE FROM SATLANTIC SPECIFICATIONS CONVERT TO PYTHON timedelta
@@ -946,7 +946,7 @@ class Instrument:
             # warnings('Fit type processing should not be applied to a NONE sensor.')
             return None
         else:
-            raise ParserFitError(f"Fit type {fit_type} not supported.")
+            raise ParserFitError("Fit type {} not supported.".format(fit_type))
 
     @staticmethod
     def compute_check_sum(frame, check_sum_index=-3):
@@ -1007,7 +1007,7 @@ class Instrument:
                             # Parse timestamp
                             ts = unpack('!ii', b'\x00' + ts)
                             try:
-                                timestamp = datetime.strptime(f'{ts[0]}{ts[1]:09d}000', '%Y%j%H%M%S%f')
+                                timestamp = datetime.strptime('{}{:09d}000', '%Y%j%H%M%S%f'.format(ts[0], ts[1]))
                             except ValueError as e:
                                 warnings.warn('Time Impossible, frame likely corrupted.')
                                 timestamp = 'NaN'
@@ -1190,7 +1190,7 @@ class SatViewRawToCSV:
         except FrameHeaderNotFoundError:
             if frame_header not in self.missing_frame_header:
                 self.missing_frame_header.append(frame_header)
-                warnings.warn(f'Missing calibration file for: {frame_header}')
+                warnings.warn('Missing calibration file for: {}'.format(frame_header))
             return
         except FrameLengthError as e:
             print(e)
@@ -1200,12 +1200,12 @@ class SatViewRawToCSV:
         # Write data
         if self.instrument.cal[frame_header].core_variables:
             data = next(iter(parsed_frame.values())).tolist()
-            data = ["%.10f" % v for v in data]
+            data = ['%.10f' % v for v in data]
             for k in [k for i, k in enumerate(self.instrument.cal[frame_header].key) if
                       i in self.instrument.cal[frame_header].auxiliary_variables]:
                 if k in parsed_frame.keys():
                     if isinstance(parsed_frame[k], float):
-                        data.append('%.4f' % parsed_frame[k])
+                        data.append(':.4f'.format(parsed_frame[k]))
                     else:
                         data.append(str(parsed_frame[k]))
         else:
@@ -1213,7 +1213,7 @@ class SatViewRawToCSV:
             for k in self.instrument.cal[frame_header].key:
                 if k in parsed_frame.keys():
                     if isinstance(parsed_frame[k], float):
-                        data.append('%.4f' % parsed_frame[k])
+                        data.append(':.4f'.format(parsed_frame[k]))
                     else:
                         data.append(str(parsed_frame[k]))
         self.w[frame_header].write([timestamp] + data)
